b0dee2362e06c86f72c983deaa786793dbe8f97c
Colleen Blake <colleenemilyblake@gmail.com>
2023-07-16 20:31:09 -0500

    Without style

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,12 +1,18 @@
-//import satData
+import satData from "./satData";
 
 
-const Buttons = () => {
+const Buttons = ({filterByType, setSat, displaySats}) => {
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-      </div>
+      {displaySats.map((sat, id) => {
+        return (
+          <button onClick={() => filterByType(sat)} key={id}>
+            {sat} Orbit
+          </button>
+        );
+      })}:
+      <button onClick={() => setSat(satData)}>All Orbits</button>
+    </div>
   );
 };
 
